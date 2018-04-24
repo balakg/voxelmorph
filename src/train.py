bdd8bc1bc210c921af0604ccf8f1dbe814f23e26
Guha Balakrishnan <balakg@twix.csail.mit.edu>
2018-04-24 16:32:37 -0400

    command line parsing

--- src/train.py
+++ src/train.py
@@ -7,6 +7,7 @@ import os
 import glob
 import sys
 import random
+from argparse import ArgumentParser
 
 # third-party imports
 import tensorflow as tf
@@ -23,36 +24,24 @@ import datagenerators
 import networks
 import losses
 
-# configuration
-n_iterations = 200001
-lr = 1e-4
-reg_param = 1.0
-model_save_iter = 5000
-
-# UNET filters
-nf_enc = [16, 32, 32, 32]
-# VM-1
-nf_dec = [32, 32, 32, 32, 8, 8, 3]
-# VM-2
-#nf_dec = [32,32,32,32,32,16,16,3]
 
 vol_size = (160, 192, 224)
-base_data_dir = '/insert/your/path/here/'
+#base_data_dir = '/insert/your/path/here'
+#base_data_dir = '/data/vision/polina/projects/ADNI/work/neuron/data/t1_mix/proc/resize256-crop_x32/'
 train_vol_names = glob.glob(base_data_dir + 'train/vols/*.npz')
 random.shuffle(train_vol_names)
 
-#val_vol_names = glob.glob(base_data_dir + 'test/vols/*.npz')
+#val_vol_names = glob.glob(base_data_dir + 'validate/vols/*.npz')
 #train_seg_dir = base_data_dir + 'train/asegs/'
-#val_seg_dir = base_data_dir + 'test/asegs/'
+#val_seg_dir = base_data_dir + 'validate/asegs/'
 
 atlas = np.load('../data/atlas_norm.npz')
 atlas_vol = atlas['vol']
 atlas_vol = np.reshape(atlas_vol, (1,) + atlas_vol.shape+(1,))
 
+def train(model,save_name, gpu_id, lr, n_iterations, reg_param, model_save_iter):
 
-def train(model_name, gpu_id):
-
-    model_dir = '../models/' + model_name
+    model_dir = '../models/' + save_name
     if not os.path.isdir(model_dir):
         os.mkdir(model_dir)
 
@@ -63,6 +52,14 @@ def train(model_name, gpu_id):
     config.allow_soft_placement = True
     set_session(tf.Session(config=config))
 
+
+    # UNET filters
+    nf_enc = [16,32,32,32]
+    if(model == 'vm1'):
+        nf_dec = [32,32,32,32,8,8,3]
+    else:
+        nf_dec = [32,32,32,32,32,16,16,3]
+
     with tf.device(gpu):
         model = networks.unet(vol_size, nf_enc, nf_dec)
         model.compile(optimizer=Adam(lr=lr), loss=[
@@ -72,9 +69,9 @@ def train(model_name, gpu_id):
     train_example_gen = datagenerators.example_gen(train_vol_names)
     zero_flow = np.zeros((1, vol_size[0], vol_size[1], vol_size[2], 3))
 
-    for step in xrange(0, n_iterations):
+    for step in range(0, n_iterations):
 
-        X = train_example_gen.next()[0]
+        X = train_example_gen.__next__()[0]
         train_loss = model.train_on_batch(
             [X, atlas_vol], [atlas_vol, zero_flow])
 
@@ -91,17 +88,33 @@ def printLoss(step, training, train_loss):
     s = str(step) + "," + str(training)
 
     if(isinstance(train_loss, list) or isinstance(train_loss, np.ndarray)):
-        for i in xrange(len(train_loss)):
+        for i in range(len(train_loss)):
             s += "," + str(train_loss[i])
     else:
         s += "," + str(train_loss)
 
-    print s
+    print(s)
     sys.stdout.flush()
 
 
 if __name__ == "__main__":
-    if(len(sys.argv) != 3):
-        print "Need model name and gpu id as command line arguments."
-    else:
-        train(sys.argv[1], sys.argv[2])
+
+    parser = ArgumentParser()
+    parser.add_argument("--model", type=str, 
+						dest="model", choices=['vm1','vm2'],
+						default='vm2',help="Voxelmorph-1 or 2")
+    parser.add_argument("--save_name", type=str,required=True,
+						dest="save_name", help="Name of model when saving")
+    parser.add_argument("--gpu", type=int,default=0,
+						dest="gpu_id", help="gpu id number")
+    parser.add_argument("--lr", type=float, 
+						dest="lr", default=1e-4,help="learning rate") 
+    parser.add_argument("--iters", type=int, 
+						dest="n_iterations", default=150000,help="number of iterations")
+    parser.add_argument("--lambda", type=float, 
+						dest="reg_param", default=1.0,help="regularization parameter")
+    parser.add_argument("--checkpoint_iter", type=int,
+						dest="model_save_iter", default=5000, help="frequency of model saves")
+
+    args = parser.parse_args()
+    train(**vars(args))
